d8b32b24addd1b06f2f3f064b5f6e1d987579755
Thykof <nathan.seva@outlook.fr>
2018-09-01 21:00:31 +0200

    Add popup

--- wordtranslate.js
+++ wordtranslate.js
@@ -1,77 +1,35 @@
-browser.menus.create(
-	{
-		id: "wordtranslate",
-		title: "wordtranslate",
-		contexts: ["selection"]
-	}
-);
+var form = document.getElementById("form-config");
+form.addEventListener("submit", changeOutput, false);
 
-browser.menus.create(
-	{
-		id: "result",
-		title: "[result here]",
-		contexts: ["selection"]
-	}
-);
+function changeOutput() {
+  var newLang = document.getElementById("lang").value;
+  window.OutputLang = newLang;
+  var text = "un message du script de la popup vers le content_scripts";
 
-browser.menus.create(
-	{
-		id: "Yandex",
-		title: "Powered by Yandex.Translate",
-		contexts: ["selection"]
-	}
-);
+  // sendToContentScript(text);
+}
 
-// async function fetchAsync (url) {
-//   let response = await fetch(url);
-//   let data = await response.json();
-//   return data;
+// browser.runtime.onMessage.addListener((message) => {
+//   alert("message recu!");
+//   document.getElementById("result").innerHTML = message.result;
+// });
+
+// function addNode(text) {
+//   var node = document.createElement("P");
+//   var textnode = document.createTextNode(text);
+//   node.appendChild(textnode);
+//   var errorDiv = document.getElementById("error");
+//   errorDiv.appendChild(node);
+//
+//   // var errorDiv = document.getElementById("error-p");
+//   // errorDiv.innerHTML += "changeOutput";
 // }
 
-function fetchSync (url) {
-	fetch(url).then(function(response) {
-		return response.json();
-	}).then(function(data) {
-		updateItem(data['text'][0]);
-	}).catch(function() {
-		console.log("Fetch error");
-	});
+
+/////////////////////// send message to content_scripts
+function sendToContentScript(text) {
+  var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+  gettingActiveTab.then((tabs) => {
+    browser.tabs.sendMessage(tabs[0].id, {message: text});
+  });
 }
-
-
-function updateItem (new_title) {
-	browser.menus.update("result", {
-		title: new_title,
-    });
-}
-
-
-function getResult(text) {
-	
-	var key = "trnsl.1.1.20180901T094046Z.8c594d68da93934a.235de79b3689fd9b65f3a6d7e7fd9e314869af07";
-	var lang_code = "fr";
-
-	var url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key="+key+"&text="+text+"&lang="+lang_code; + "callback=" + "callback";
-	var data = fetchSync(url);
-}
-
-function showYandex () {
-	var creating = browser.tabs.create(
-		{
-			url: "http://translate.yandex.com/"
-		}
-	)
-}
-
-browser.menus.onClicked.addListener((info, tab) => {
-  switch (info.menuItemId) {
-    case "wordtranslate":
-			console.log("Translate");
-	  	getResult(info.selectionText);
-      break;
-    case "Yandex":
-	  	showYandex();
-      break;
-  }
-});
-
