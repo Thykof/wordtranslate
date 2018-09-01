a9b48d0e88c58788b077d7b35419ecee91569a5f
Thykof <nathan.seva@outlook.fr>
2018-09-01 18:13:41 +0200

    Clean code

--- wordtranslate.js
+++ wordtranslate.js
@@ -9,7 +9,7 @@ browser.menus.create(
 browser.menus.create(
 	{
 		id: "result",
-		title: "result...",
+		title: "[result here]",
 		contexts: ["selection"]
 	}
 );
@@ -22,34 +22,26 @@ browser.menus.create(
 	}
 );
 
-function callback(arg) {
-	console.log("callback");
-}
-
-async function fetchAsync (url) {
-  let response = await fetch(url);
-  let data = await response.json();
-  return data;
-}
+// async function fetchAsync (url) {
+//   let response = await fetch(url);
+//   let data = await response.json();
+//   return data;
+// }
 
 function fetchSync (url) {
 	fetch(url).then(function(response) {
 		return response.json();
 	}).then(function(data) {
-		console.log(data);
+		updateItem(data['text'][0]);
 	}).catch(function() {
-		console.log("Booo");
+		console.log("Fetch error");
 	});
 }
 
 
-function updateItem (result) {
-	console.log(result);
-	console.log(result['text']);
-	console.log(result['text'][0]);
-	
+function updateItem (new_title) {
 	browser.menus.update("result", {
-		title: result['text'][0],
+		title: new_title,
     });
 }
 
@@ -58,15 +50,9 @@ function getResult(text) {
 	
 	var key = "trnsl.1.1.20180901T094046Z.8c594d68da93934a.235de79b3689fd9b65f3a6d7e7fd9e314869af07";
 	var lang_code = "fr";
-	
+
 	var url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key="+key+"&text="+text+"&lang="+lang_code; + "callback=" + "callback";
 	var data = fetchSync(url);
-	var result;
-	
-	fetch(url).then(r => r.json())
-		.then(data => updateItem(data))
-		.catch(e => console.log("Booo"))
-
 }
 
 function showYandex () {
@@ -79,13 +65,12 @@ function showYandex () {
 
 browser.menus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
-    case "result":
-      console.log(info.selectionText);
-	  getResult(info.selectionText);
+    case "wordtranslate":
+			console.log("Translate");
+	  	getResult(info.selectionText);
       break;
     case "Yandex":
-	  console.log("Powered by Yandex.Translate");
-	  showYandex();
+	  	showYandex();
       break;
   }
 });
